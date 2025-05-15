bf32f1946ae8d3e2becc8d1ecf81d69c883f13bf
Hossein88-eng <beidaghydizaji@gmail.com>
2025-05-15 16:12:36 +0000

    data_loader was updated as we had no test_dataset and it is equal to the validation dataset now

--- src/data_loader.py
+++ src/data_loader.py
@@ -1,70 +1,28 @@
 import os
 import tensorflow as tf
 
-DATA_DIR = "/workspaces/gender-classification-doe/data"
+BASE_DIR = os.getcwd()
+DATA_DIR = os.path.join(BASE_DIR, "data")
 
-
-def get_image_counts():
-    """Returns the number of images for each class."""
-    data_dir_woman = os.path.join(DATA_DIR, "woman")
-    data_dir_man = os.path.join(DATA_DIR, "man")
-    return len(os.listdir(data_dir_woman)), len(os.listdir(data_dir_man))
-
-
-def load_datasets(
-    img_size, batch_size, validation_split, test_split, seed_num, shuffle_buffer_size
-):
-    """Loads training, validation, and test datasets."""
-    image_size = (img_size, img_size)
-
+def load_datasets(data_path=DATA_DIR):
     train_dataset = tf.keras.utils.image_dataset_from_directory(
-        directory=DATA_DIR,
-        image_size=image_size,
-        batch_size=batch_size,
-        label_mode="binary",
-        validation_split=validation_split + test_split,
+        data_path,
+        validation_split=0.2,
         subset="training",
-        seed=seed_num,
+        seed=123,
+        image_size=(224, 224),
+        batch_size=32
     )
 
     val_dataset = tf.keras.utils.image_dataset_from_directory(
-        directory=DATA_DIR,
-        image_size=image_size,
-        batch_size=batch_size,
-        label_mode="binary",
-        validation_split=validation_split + test_split,
+        data_path,
+        validation_split=0.2,
         subset="validation",
-        seed=seed_num,
+        seed=123,
+        image_size=(224, 224),
+        batch_size=32
     )
 
-    # Further split validation dataset to extract test dataset if needed
-    val_batches = tf.data.experimental.cardinality(val_dataset)
-    test_size = round(
-        val_batches.numpy() * (test_split / (validation_split + test_split))
-    )
-
-    test_dataset = val_dataset.take(test_size)
-    val_dataset = val_dataset.skip(test_size)
+    test_dataset = val_dataset  # or create separate test if you want
 
     return train_dataset, val_dataset, test_dataset
-
-
-def optimize_datasets(train_dataset, val_dataset, test_dataset, shuffle_buffer_size):
-    """Optimizes datasets for performance."""
-    AUTOTUNE = tf.data.AUTOTUNE
-
-    train_dataset = (
-        train_dataset.cache()
-        .shuffle(shuffle_buffer_size)
-        .prefetch(buffer_size=AUTOTUNE)
-    )
-    val_dataset = val_dataset.cache().prefetch(buffer_size=AUTOTUNE)
-    test_dataset = test_dataset.cache().prefetch(buffer_size=AUTOTUNE)
-
-    return train_dataset, val_dataset, test_dataset
-
-
-def get_example_batch(training_dataset):
-    """Retrieves the first batch of images and labels."""
-    for images, labels in training_dataset.take(1):
-        return images, labels
