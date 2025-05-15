655427af8037a3af8189818075c53ad341e6373d
Hossein88-eng <beidaghydizaji@gmail.com>
2025-05-15 16:56:43 +0000

    updated the project

--- tests/test_all.py
+++ tests/test_all.py
@@ -2,19 +2,28 @@ import os
 import unittest
 from pathlib import Path
 
-DATA_DIR = Path.cwd() / "data"
+from src.image_predictor import load_model
+from src.data_loader import load_datasets
+
+MODEL_PATH = "gender_recognition_DOE_run6.h5"
+DATA_PATH = Path.cwd() / "data"
+
+def data_available():
+    return DATA_PATH.exists()
 
 class TestProject(unittest.TestCase):
     def setUp(self):
-        if not DATA_DIR.exists():
-            self.skipTest(f"Data directory '{DATA_DIR}' not found. Skipping dataset-dependent tests.")
-        
-        from src.data_loader import load_datasets
-        from src.image_predictor import load_model, MODEL_PATH
-
-        self.train_dataset, self.val_dataset, self.test_dataset = load_datasets(DATA_DIR)
         self.model = load_model(MODEL_PATH)
+        
+        if not data_available():
+            self.skip_all_tests = True
+        else:
+            self.skip_all_tests = False
+            self.train_dataset, self.val_dataset, self.test_dataset = load_datasets()
 
-
-if __name__ == "__main__":
-    unittest.main()
+    def test_dummy(self):
+        if self.skip_all_tests:
+            self.skipTest("Skipping test because data directory not found.")
+        
+        # Otherwise perform a real test
+        self.assertIsNotNone(self.model)
